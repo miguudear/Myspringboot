2c074a58900aaf36e23a8b5ebe52d59ce6182682
migu <1328729385@qq.com>
2020-03-12 11:56:45 +0800

    第15次提交

--- src/main/java/com/lw/springboot/HelloController.java
+++ src/main/java/com/lw/springboot/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello Spring Boot!14 中国人！";
+        return "Hello Spring Boot!15 中国人！";
     }
 }
