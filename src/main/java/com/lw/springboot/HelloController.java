72140b7bc776b9ac153b60fb9146bcc132fa159a
lw2 <2@qq.com>
2020-03-13 17:25:52 +0800

    第23次修改

--- src/main/java/com/lw/springboot/HelloController.java
+++ src/main/java/com/lw/springboot/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello Spring Boot!22 中国人！";
+        return "Hello Spring Boot!23 中国人！";
     }
 }
